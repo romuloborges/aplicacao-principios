99362020cbe5d58e50b9442eb67ee7144c3474cb
Rômulo Borges <romuloborges111@gmail.com>
2020-10-18 21:31:32 -0300

    LiskovSubstitutionPrinciple finalizado

---
+++ LiskovSubstitutionPrinciple/src/main/java/br/ufes/lsp/errado/collection/CriaturaCollection.java
@@ -0,0 +1,36 @@
+package br.ufes.lsp.errado.collection;
+
+import br.ufes.lsp.errado.model.Criatura;
+import br.ufes.lsp.errado.model.Jogador;
+import br.ufes.lsp.errado.model.Inimigo;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author rborges
+ */
+public class CriaturaCollection {
+    
+    private final ArrayList<Criatura> criaturas = new ArrayList<>();
+    private static CriaturaCollection instancia;
+    
+    public static CriaturaCollection getInstancia() {
+        if (instancia == null) {
+            instancia = new CriaturaCollection();
+        }
+        
+        return instancia;
+    }
+    
+    private CriaturaCollection() {
+        criaturas.add(new Jogador(10, 20, 64, 64, 100));
+        criaturas.add(new Inimigo(50, 100, 64, 48, 100));
+    }
+    
+    public List<Criatura> getCriaturas() {
+        return Collections.unmodifiableList(criaturas);
+    }
+    
+}
